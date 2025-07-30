90fde26ba2b06bc07d0cd15eab0bb194e8886af9
veravasilyeva <vera.vasilyeva@mail.ru>
2025-07-30 10:25:08 +0300

    Создаем главное окно и метку и кнопку

--- Dogs.py
+++ Dogs.py
@@ -1,10 +1,25 @@
-from cProfile import label
 from tkinter import *
-from tkinter.messagebox import showinfo
-
+from tkinter import messagebox as mb
 import requests
 from PIL import ImageTk, Image
 from io import BytesIO
+
+
+
+def show_image():
+    image_url = get_doc_image()
+    if image_url:
+        try:
+            response = requests.get(image_url, stream=True)
+            response.raise_for_status()
+            img_data = BytesIO(response.content)
+            img = Image.open(img_data)
+            img = img.thumbnail((300,300))
+            label.config(image=img)
+            label.image = img
+        except Exception as e:
+            mb.showerror("Ошибка", f"Возникла ошибка {e}")
+
 
 
 
